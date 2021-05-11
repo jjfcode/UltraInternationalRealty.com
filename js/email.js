09e3c9781516910167060166fdc2761de8a8788c
John Ferlito <mix3@mix3sound.com>
2021-05-11 17:54:41 -0400

    Fix Form

--- js/email.js
+++ js/email.js
@@ -7,6 +7,7 @@ function check() {
     var choose = document.getElementById('inputState').value;
     var zip = document.getElementById('inputZip').value;
     var problem = document.getElementById('inputTop').value;
+    var date = document.getElementById('inputDate').value;
     var explain = document.getElementById('exampleFormControlTextarea1').value;
 
     if(name==false) {
@@ -33,6 +34,9 @@ function check() {
     } else if(problem==false) {
         alert('Please Select Type of Problem');
         return false;
+    } else if(date==false) {
+        alert('Date is Required');
+        return false;
     } else if(explain==false) {
         alert('Explain is Required');
         return false;
@@ -51,13 +55,14 @@ function send(event) {
         From : document.getElementById('inputEmail4').value,
         Subject : 'Request a Quote',
         Body : "<h3 style='display:inline;'>Name: </h3>" + document.getElementById('fullName').value +
-        "<br><h3 style='display:inline;'>Email: </h3>" + document.getElementById('email').value + 
+        "<br><h3 style='display:inline;'>Email: </h3>" + document.getElementById('inputEmail4').value + 
         "<br><h3 style='display:inline;'>Phone: </h3>" + document.getElementById('phone').value +
         "<br> <h3 style='display:inline;'>Address: </h3>" + document.getElementById('inputAddress').value + 
         "<br> <h3 style='display:inline;'>City: </h3>" + document.getElementById('inputCity').value + 
         "<br> <h3 style='display:inline;'>State: </h3>" + document.getElementById('inputState').value + 
         "<br> <h3 style='display:inline;'>Zip Code: </h3>" + document.getElementById('inputZip').value + 
         "<br> <h3 style='display:inline;'>Type Of Problem: </h3>" + document.getElementById('inputTop').value +
+        "<br> <h3 style='display:inline;'>Date Problem Start: </h3>" + document.getElementById('inputDate').value +
         "<br><h3 style='display:inline;'>Explain: </h3>" + document.getElementById('exampleFormControlTextarea1').value,
         }).then(function(response){
             if (response == 'OK') {
